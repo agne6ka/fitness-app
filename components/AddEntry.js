80d38b08d256e34f577a2c7a86f5968e7bced9ed
Agnieszka Wypich <agne6ka@gmail.com>
2020-04-01 18:14:30 +0200

    feat: Add api with async storage to save the results.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import StepSlider from "./StepSlider";
 import Stepper from "./Stepper";
 import { Ionicons } from "@expo/vector-icons";
 import TextButton from "./TextButton";
+import { submitEntry, removeEntry } from "../utils/api";
 
 function SubmitBtn({ onPress }) {
   return (
@@ -60,7 +61,7 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    // Save to "DB"
+    submitEntry({ key, entry });
 
     // Clear local notification
   };
@@ -71,7 +72,7 @@ export default class AddEntry extends Component {
 
     // Route to Home
 
-    // Update "DB"
+    removeEntry(key);
   };
   render() {
     const metaInfo = getMetricMetaInfo();
